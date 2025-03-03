2a4c527799f3b6c2c9eb3d4fb29c87f20df88427
SDSDASDASDSAD <stanley6839@gmail.com>
2025-03-03 15:40:23 +0800

    1

--- index.php
+++ index.php
@@ -6,6 +6,15 @@
     <title>測驗1</title>
 </head>
 <body>
-    
-</body>
+    <h1>我的練習 php & mysq1</h1>
+
+    <p>今天是: <?=date("Y年m月d日") ?></p>
+    <hr>
+    <p>我的名字: <?= $_GET["name"] ?></p>
+    <p>我的年紀: <?= $_GET["age"] ?></p>
+
+    <p>我的身高: <?= $h=$_GET["height"] ?></p>
+    <p>我的體重: <?= $w=$_GET["weight"] ?></p>
+    <p>我的bmi值: <?= $w/($h/100*$h/100) ?></p>
+    </body>
 </html>
